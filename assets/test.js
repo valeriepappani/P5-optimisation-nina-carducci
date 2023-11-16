2515868b446fc0b2eb81e0f9099153accb823eda
Valerie Pappani <v.pappani@smallstories.fr>
2023-11-16 15:41:08 +0100

    modifications images

---
+++ assets/test.js
@@ -0,0 +1,10 @@
+// const filtres = document.querySelector(".nav-link");
+// filtres.forEach(filtre => {
+//     filtre.addEventListener("click", function() {
+//         filtre.classList.add("active");
+//     });
+// })
+
+
+const test = document.querySelector(".nav-pills");
+console.log(test)
